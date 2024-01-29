e6779ae0e72db5451c073028c877859c214b7366
Alan Magliolini <alannm54@gmail.com>
2024-01-29 00:18:05 -0300

    finishing the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Box } from '@chakra-ui/react'
-import { HashRouter, BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter,Route, Routes } from 'react-router-dom'
 import Home from './components/pages/home/Home'
 import BookDetail from './components/pages/bookDetail/BookDetail'
 import NotFoundPage from './components/pages/notFoundPage/NotFoundPage'
@@ -14,24 +13,12 @@ function App() {
 
         <Route path='/book/:bookId' element={<BookDetail/>}/>
 
-//      <Route path='*' element={<NotFoundPage />}/>
+        <Route path='*' element={<NotFoundPage />}/>
 
       </Routes>
     </HashRouter>
   
   )
-    //  <BrowserRouter>
-    //    <Routes>
-
-    //      <Route path='/' element={<Home />}/>
-         
-    //      <Route path='/book/:bookId' element={<BookDetail/>}/>
-
-    //      <Route path='*' element={<NotFoundPage />}/>
-         
-    //    </Routes>
-    //  </BrowserRouter>
-  // )
 }
 
 export default App
